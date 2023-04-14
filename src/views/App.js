a3406161c22efb2345a03cd95471eeaca6ed5e40
STS <hieunguyen.13092000@gmail.com>
2023-04-15 06:36:12 +0700

    Commit second - example state

--- src/views/App.js
+++ src/views/App.js
@@ -1,7 +1,18 @@
 import logo from './logo.svg';
 import './App.scss';
 
+import MyComponent from './Example/MyComponent.js';
+
+/*
+* Have 2 type component: class component /  function component (function, arrow)
+*
+* Class component react
+* JSX : là một function, hay một hàm của JavaScript, trả về một đống HTML. Code JavaScript trong HTML
+*
+*/
+
 function App() {
+  // c2, khai báo arrow component  const App = () => { return (...)}
   return (
     <div className="App">
       <header className="App-header">
@@ -18,6 +29,15 @@ function App() {
         >
           Learn React
         </a>
+        {/* crtl + / để cmt */}
+        {/* loại này hay được dùng */}
+
+        {/* nhớ là muốn sử dụng ở đây thì file MyComponent, class MyComponent đã được export */}
+        <MyComponent />
+
+        {/* Loại này đc dùng khi muốn render cái con của cái Mycomponent */}
+        {/* <MyComponent>  </MyComponent> */}
+
       </header>
     </div>
   );
---
+++ src/views/Example/MyComponent.js
@@ -0,0 +1,60 @@
+import React from 'react';
+
+// đặt tên class giống tên file
+
+class MyComponent extends React.Component {
+
+    /* shift + Alt + A để tạo ra cmt khối */
+
+    /* 
+    *
+    * JSX : return block
+    * tragment:
+    * state: những dữ liệu có thể thay đổi, giúp cần cập nhập dữ liệu mà không cần phải load lại trang. Khi giá trị của state thay đổi thì JSX tự động thay đổi. 
+    * 
+    */
+    // KEY: VALUE
+    // state như một object
+    state = {
+        name: 'Hieu',
+        channel: 'sts_Hieu'
+    }
+
+    // trong vi du duoi kia
+    handleOnChangeName = (event) => {
+        this.setState({
+            name: event.target.value
+        })
+    }
+
+    render() {
+        //let name = 'Hieu';
+
+        // hàm return phải return về MỘT khối code. 
+        return (
+            <>
+                <div className='first'>
+                    {/* khi muốn dùng javaScript thì mở ngoặc nhọn {} */}
+                    {/* Hello my Component, My name is {this.state.name} */}
+
+                    {/* một ví dụ nhỏ về state */}
+
+                    <input value={this.state.name} type='text'
+                        onChange={(event) => this.handleOnChangeName(event)}
+                    />
+                    My name is {this.state['name']}
+                    {/* CACH 2 */}
+                    {/* Hello my Component, My name is {this.state['name']} */}
+                </div>
+
+                <div className='second'>
+                    My tiktok channel : {this.state.channel}
+                </div>
+            </>
+        )
+    }// end component
+}
+
+
+// khi muốn sử dụng cái class này để đến cái file khác sử dụng thì export nó
+export default MyComponent;
